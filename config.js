abeb74e7fd007746e2d0f566343c9fc1116ac55c
malik.imenov <imenov-a@mail.ru>
2022-10-23 02:27:00 +0600

    init

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // nodemailer outlook settings
 exports.SMTP_PORT = 25
-exports.SMTP_HOST = '10.185.232.39'
-exports.SMTP_USER = 'abdumalik.imenov@optimabank.kg'
-exports.SMTP_TITLE_URL = 'http://localhost:3000/api/mail/message'
-exports.SMTP_TO_MAIL = 'abdumalik.imenov@optimabank.kg'
+exports.SMTP_HOST = ''
+exports.SMTP_USER = ''
+exports.SMTP_TITLE_URL = ''
+exports.SMTP_TO_MAIL = ''
 
 // rabbit api
-exports.RABBIT_URL = 'amqp://guest:guest@10.185.233.241:5672'
-exports.RABBIT_URL_ADMIN = 'http://guest:guest@10.185.233.241:15672/api/queues'
+exports.RABBIT_URL = ''
+exports.RABBIT_URL_ADMIN = ''
 exports.DEV_PORT = 3000
